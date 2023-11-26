f91d64af5245a81711b70804432edfa31050c926
XIMI <145792678+munume@users.noreply.github.com>
2023-11-26 21:40:58 +0800

    Update 上传图片.py

--- flask/zym/上传图片.py
+++ flask/zym/上传图片.py
@@ -4,6 +4,10 @@ from flask import Flask, request, jsonify
 from datetime import datetime
 from werkzeug.utils import secure_filename
 from gevent import pywsgi
+import glob
+import numpy as np
+from PIL import Image
+import scipy.io as io
 
 # 获取当前位置的绝对路径
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -21,18 +25,42 @@ def upload():
     random_num = random.randint(0, 100)
     # 获取文件的后缀
     filename = datetime.now().strftime("%Y%m%d%H%M%S") + "_" + str(random_num) + "." + filename.rsplit('.', 1)[1]
-    if not os.path.exists(filename):
-        os.makedirs(filename, 755)
+    # if not os.path.exists(filename):
+    #     os.makedirs(filename, 755)
     file_path = basedir + "/static/file/" + filename
     f.save(file_path)
     # 返回前端可调用的一个链接
     # 可以配置成对应的外网访问的链接
     my_host = "http://127.0.0.1:8080"
     new_path_file = my_host + "/static/file/" + filename
+
     data = {"msg": "success", "imageURL": new_path_file}
 
     payload = jsonify(data)
+
+    save_dir = basedir + "/static/file/"
+    print('Start...')
+    img = Image.open(file_path)
+    # save to .npy
+    res = np.array(img, dtype='uint16')
+    print('Saving data...')
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+    np.save(save_dir + 'origin.npy', res)
+    print('Done.')
+
+    numpy_file = np.load(save_dir+'/origin.npy')
+
+    io.savemat('origin.mat', {'data': numpy_file})
+
     return payload, 200
+
+
+#
+# @app.route("/change", method=["POST"])
+# def change():
+#     my_host = "http: // 127.0.0.1: 8080 / change"
+#     data = {"msg": "success", "imageURL": new_path_file}
 
 
 if __name__ == '__main__':
