73ff717cccc17147bab80ba3e484d9c86bfea79f
huaji <104162106+huaaowu@users.noreply.github.com>
2024-01-11 23:58:50 +0800

    Update hybridsn_bn.py

--- HybridSN_BN/hybridsn_bn.py
+++ HybridSN_BN/hybridsn_bn.py
@@ -106,79 +106,7 @@ def splitTrainTestSet(X, y, testRatio, randomState=345):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=testRatio, random_state=randomState, stratify=y)
     return X_train, X_test, y_train, y_test
 
-# 地物类别
-class_num = 16
-X = sio.loadmat('Indian_pines_corrected.mat')['indian_pines_corrected']
-y = sio.loadmat('Indian_pines_gt.mat')['indian_pines_gt']
 
-# 用于测试样本的比例
-test_ratio = 0.90
-# 每个像素周围提取 patch 的尺寸
-patch_size = 25
-# 使用 PCA 降维，得到主成分的数量
-pca_components = 30
-
-print('Hyperspectral data shape: ', X.shape)
-print('Label shape: ', y.shape)
-
-print('\n... ... PCA tranformation ... ...')
-X_pca = applyPCA(X, numComponents=pca_components)
-print('Data shape after PCA: ', X_pca.shape)
-
-print('\n... ... create data cubes ... ...')
-X_pca, y = createImageCubes(X_pca, y, windowSize=patch_size)
-print('Data cube X shape: ', X_pca.shape)
-print('Data cube y shape: ', y.shape)
-
-print('\n... ... create train & test data ... ...')
-Xtrain, Xtest, ytrain, ytest = splitTrainTestSet(X_pca, y, test_ratio)
-print('Xtrain shape: ', Xtrain.shape)
-print('Xtest  shape: ', Xtest.shape)
-
-# 改变 Xtrain, Ytrain 的形状，以符合 keras 的要求
-Xtrain = Xtrain.reshape(-1, patch_size, patch_size, pca_components, 1)
-Xtest  = Xtest.reshape(-1, patch_size, patch_size, pca_components, 1)
-print('before transpose: Xtrain shape: ', Xtrain.shape)
-print('before transpose: Xtest  shape: ', Xtest.shape)
-
-# 为了适应 pytorch 结构，数据要做 transpose
-Xtrain = Xtrain.transpose(0, 4, 3, 1, 2)
-Xtest  = Xtest.transpose(0, 4, 3, 1, 2)
-print('after transpose: Xtrain shape: ', Xtrain.shape)
-print('after transpose: Xtest  shape: ', Xtest.shape)
-
-
-""" Training dataset"""
-class TrainDS(torch.utils.data.Dataset):
-    def __init__(self):
-        self.len = Xtrain.shape[0]
-        self.x_data = torch.FloatTensor(Xtrain)
-        self.y_data = torch.LongTensor(ytrain)
-    def __getitem__(self, index):
-        # 根据索引返回数据和对应的标签
-        return self.x_data[index], self.y_data[index]
-    def __len__(self):
-        # 返回文件数据的数目
-        return self.len
-
-""" Testing dataset"""
-class TestDS(torch.utils.data.Dataset):
-    def __init__(self):
-        self.len = Xtest.shape[0]
-        self.x_data = torch.FloatTensor(Xtest)
-        self.y_data = torch.LongTensor(ytest)
-    def __getitem__(self, index):
-        # 根据索引返回数据和对应的标签
-        return self.x_data[index], self.y_data[index]
-    def __len__(self):
-        # 返回文件数据的数目
-        return self.len
-
-# 创建 trainloader 和 testloader
-trainset = TrainDS()
-testset  = TestDS()
-train_loader = torch.utils.data.DataLoader(dataset=trainset, batch_size=128, shuffle=True, num_workers=2)
-test_loader  = torch.utils.data.DataLoader(dataset=testset,  batch_size=128, shuffle=False, num_workers=2)
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 net = HybridSN_BN()
@@ -186,6 +114,7 @@ net = HybridSN_BN()
 net.load_state_dict(torch.load('HybridSN_BN.pth',map_location='cpu'))
 net.to(device)
 net.eval()
+
 def pred(X,y,result_path):
 
   height = y.shape[0]
@@ -211,20 +140,3 @@ def pred(X,y,result_path):
         print('... ... row ', i, ' handling ... ...')
   predict_image = spectral.imshow(classes = outputs.astype(int),figsize =(5,5))
   plt.savefig(result_path)#默认png格式
-
-# source_1_path = 'media/source' + ''.join(str(mat1).split('.')[:-1]) + '.mat'
-# source_2_path = 'media/source' + ''.join(str(mat2).split('.')[:-1]) + '.mat'
-X = sio.loadmat('Indian_pines_corrected.mat')['indian_pines_corrected']#遥感图像
-y = sio.loadmat('Indian_pines_gt.mat')['indian_pines_gt']#每个像素的地物类别标签
-# X = sio.loadmat('PaviaU.mat')['paviaU']
-# y = sio.loadmat('PaviaU_gt.mat')['paviaU_gt']
-# source1='/content/Indian_pines_corrected.mat.2'
-# source2='/content/Indian_pines_gt.mat.2'
-#result_path = 'content/' + ''.join(str(X).split('.')[:-1]) + '.png'
-result_path="/content/result_HybridSN_BN"
-# result = test_map(source1,source2,result_path)
-pred(X,y,result_path)
-# drive.mount('/content/drive')
-
-# os.chdir('/content/drive/My Drive/')
-# drive.mount("/content/drive", force_remount=True)
